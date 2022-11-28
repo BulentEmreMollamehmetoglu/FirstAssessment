c307119151c2ae0ac8e1dcce4e14f4ab367d97c7
BulentEmreMollamehmetoglu <106030451+BulentEmreMollamehmetoglu@users.noreply.github.com>
2022-11-28 12:39:57 +0300

    ters ucgen

---
+++ src/ThirdAssesment/TersUcgen.java
@@ -0,0 +1,26 @@
+package ThirdAssesment;
+
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("sayı giriniz:");
+        int n = inp.nextInt();
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int k = n; k > i; k--) {
+                System.out.print(" ");
+            }
+
+            for (int l = 1; l <= (2 * i - 1); l++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+
+        }
+
+
+    }
+}
